0f935e65550ccdc5cae3cc64919de0e87ec64384
mirabzal-07 <mirabzalozodov07@gmail.com>
2022-07-18 00:06:41 +0500

    online chat

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000";
+export const host = "https://mirafzal-online-chat.herokuapp.com";
 export const loginRoute = `${host}/user/login`;
 export const registerRoute = `${host}/user/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
